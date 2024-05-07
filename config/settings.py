a734f3e7a69e68baa34a0a16e35fa69ddd751a80
Alexandr <ale88andr@gmail.com>
2024-05-07 09:33:22 +0000

    add options for static files in settings.py

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,17 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+##############################
+# STATIC FILES SETTINGS
+##############################
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
