517dde9653152fd6ba9133575e4e7c86f9ddbcbb
Alexandr <ale88andr@gmail.com>
2024-05-08 07:23:58 +0000

    added departments model to employees app

--- employees/admin.py
+++ employees/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
+from employees.models.departments import Department
 from employees.models.organisations import Organisation
 from employees.models.structures import Structure
+
+
+admin.site.empty_value_display = "(Нет данных)"
 
 
 @admin.register(Organisation)
@@ -12,3 +16,14 @@ class OrganisationAdmin(admin.ModelAdmin):
 @admin.register(Structure)
 class OrganisationStructureAdmin(admin.ModelAdmin):
     list_display = ('name', 'organisation')
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'structure', 'structure_organisation')
+    list_select_related = ('structure', 'structure__organisation')
+    list_filter = ['structure', 'structure__organisation']
+
+    @admin.display(description="Организация")
+    def structure_organisation(self, obj):
+        return obj.structure.organisation
