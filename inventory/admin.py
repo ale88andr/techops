05d23a8f60320e5c1c14c127b2a81591110244e2
Alexandr <ale88andr@gmail.com>
2024-05-13 10:29:59 +0000

    [#0003] inventory app: improove equipment manufacturer model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from inventory.models.equipment_manufacturer import EquipmentManufacturer
 from inventory.models.equipment_type import EquipmentType
@@ -10,4 +11,10 @@ class EquipmentTypeAdmin(admin.ModelAdmin):
 
 @admin.register(EquipmentManufacturer)
 class EquipmentManufacturerAdmin(admin.ModelAdmin):
-    list_display = ('name', )
+    list_display = ('name', 'display_logo', )
+    list_display_links = ('name', 'display_logo', )
+    readonly_fields = ['display_logo']
+
+    @admin.display(description='Логотип')
+    def display_logo(self, obj):
+        return obj.display_logo
--- inventory/models/equipment_manufacturer.py
+++ inventory/models/equipment_manufacturer.py
@@ -1,6 +1,8 @@
 import os
 
 from django.db import models
+from django.utils.functional import cached_property
+from django.utils.html import format_html
 
 
 # @TODO Move to settings or env and rename
@@ -26,11 +28,18 @@ class EquipmentManufacturer(models.Model):
         blank=True
     )
 
+    # @TODO remove hardcoded values
+    @cached_property
+    def display_logo(self):
+        if self.logo:
+            return format_html(f'<img src="{self.logo.url}" width="45">')
+        return format_html('<strong>Изображение отсутствует<strong>')
+
     def __str__(self):
         return self.name
 
     class Meta:
         db_table = 'inventory_equipment_manufacturer'
-        verbose_name = 'Производитель оборудования'
+        verbose_name = 'Производителя оборудования'
         verbose_name_plural = 'Производители оборудования'
         ordering = ('name', )
