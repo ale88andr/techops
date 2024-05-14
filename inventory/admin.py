e4dcb95f2a3eea81a4b415f781770e6d240d26bc
Alexandr <ale88andr@gmail.com>
2024-05-14 08:14:07 +0000

    [feature] equipment app: add link to change owner

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from inventory.models.equipment import Equipment
 from inventory.models.equipment_manufacturer import EquipmentManufacturer
@@ -32,7 +34,7 @@ class EquipmentModelAdmin(admin.ModelAdmin):
 
 @admin.register(Equipment)
 class EquipmentAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'owner', 'status')
+    list_display = ('__str__', 'owner_link', 'status')
     readonly_fields = ('created_at', 'updated_at', 'created_by')
     list_select_related = ('owner', 'equipment_model')
     search_fields = ['inv', 'serial', ]
@@ -41,3 +43,7 @@ class EquipmentAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.created_by = request.user
         super().save_model(request, obj, form, change)
+
+    def owner_link(self, obj):
+        link = reverse('admin:employees_worker_change', args=[obj.owner.id])
+        return format_html('<a href="{}">{}</a>', link, obj.owner)
